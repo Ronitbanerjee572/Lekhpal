4a152e52024cfb0989a1eb8b0ce5354379b1135b
Argha Maity <arghamaity@Arghas-MacBook-Air.local>
2026-01-14 20:17:55 +0530

    add new file ProtectedRoute and changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Auth from './pages/Auth';
 import UserDashboard from './pages/UserDashboard';
 import GovDashboard from './pages/GovDashboard';
 import Profile from './pages/Profile';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -12,9 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/dashboard" element={<UserDashboard />} />
-        <Route path="/gov-dashboard" element={<GovDashboard />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<ProtectedRoute><UserDashboard /></ProtectedRoute>} />
+        <Route path="/gov-dashboard" element={<ProtectedRoute><GovDashboard /></ProtectedRoute>} />
+        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
   );
